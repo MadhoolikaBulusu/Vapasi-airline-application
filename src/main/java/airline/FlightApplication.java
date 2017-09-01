b0cbb3f402781b51d0e130a7a62bf6c2c1219825
Prachi Srivastava <prachi@swecha.net>
2017-09-01 16:32:20 +0530

    Moved spring boot application bootstrap from controller to application

---
+++ src/main/java/airline/FlightApplication.java
@@ -0,0 +1,13 @@
+package airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class FlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightApplication.class, args);
+    }
+}
